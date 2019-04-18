c8c8bc680958292cfe8b455ebed4e4585fd030e5
Stephen Gold <sgold@sonic.net>
2019-04-18 16:50:08 -0700

    added the ReverseAnimation demo app

---
+++ WesExamples/src/main/java/jme3utilities/test/wes/ReverseAnimation.java
@@ -0,0 +1,340 @@
+/*
+ Copyright (c) 2019, Stephen Gold
+ All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the copyright holder nor the names of its contributors
+ may be used to endorse or promote products derived from this software without
+ specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package jme3utilities.test.wes;
+
+import com.jme3.animation.AnimChannel;
+import com.jme3.animation.AnimControl;
+import com.jme3.animation.Animation;
+import com.jme3.animation.SkeletonControl;
+import com.jme3.animation.Track;
+import com.jme3.app.StatsAppState;
+import com.jme3.audio.openal.ALAudioRenderer;
+import com.jme3.input.KeyInput;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.plugins.ogre.MaterialLoader;
+import com.jme3.scene.plugins.ogre.MeshLoader;
+import com.jme3.scene.shape.Box;
+import com.jme3.shadow.DirectionalLightShadowRenderer;
+import com.jme3.system.AppSettings;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jme3utilities.Misc;
+import jme3utilities.MyAsset;
+import jme3utilities.MySpatial;
+import jme3utilities.debug.Dumper;
+import jme3utilities.debug.SkeletonVisualizer;
+import jme3utilities.math.MyVector3f;
+import jme3utilities.ui.ActionApplication;
+import jme3utilities.ui.CameraOrbitAppState;
+import jme3utilities.ui.InputMode;
+import jme3utilities.wes.TrackEdit;
+
+/**
+ * App to demonstrate reversing an animation.
+ *
+ * @author Stephen Gold sgold@sonic.net
+ */
+public class ReverseAnimation extends ActionApplication {
+    // *************************************************************************
+    // constants and loggers
+
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger
+            = Logger.getLogger(ReverseAnimation.class.getName());
+    /**
+     * application name (for the title bar of the app's window)
+     */
+    final static String applicationName
+            = ReverseAnimation.class.getSimpleName();
+    // *************************************************************************
+    // fields
+
+    /**
+     * root node of the Sinbad model
+     */
+    private Node sinbadModelRoot;
+    /**
+     * debug visualizer for Sinbad's skeleton
+     */
+    private SkeletonVisualizer sv;
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Main entry point for the application.
+     *
+     * @param ignored array of command-line arguments (not null)
+     */
+    public static void main(String[] ignored) {
+        /*
+         * Mute the chatty loggers in certain packages.
+         */
+        Misc.setLoggingLevels(Level.WARNING);
+        Logger.getLogger(ALAudioRenderer.class.getName())
+                .setLevel(Level.SEVERE);
+
+        ReverseAnimation application = new ReverseAnimation();
+        /*
+         * Customize the window's title bar.
+         */
+        AppSettings settings = new AppSettings(true);
+        settings.setTitle(applicationName);
+
+        settings.setGammaCorrection(true);
+        settings.setVSync(true);
+        application.setSettings(settings);
+
+        application.start();
+    }
+    // *************************************************************************
+    // ActionApplication methods
+
+    /**
+     * Initialize this application.
+     */
+    @Override
+    public void actionInitializeApplication() {
+        Logger.getLogger(MaterialLoader.class.getName()).setLevel(Level.SEVERE);
+        Logger.getLogger(MeshLoader.class.getName()).setLevel(Level.SEVERE);
+
+        configureCamera();
+        ColorRGBA bgColor = new ColorRGBA(0.2f, 0.2f, 1f, 1f);
+        viewPort.setBackgroundColor(bgColor);
+        addLighting();
+        stateManager.getState(StatsAppState.class).toggleStats();
+        addBox();
+        addSinbad();
+        /*
+         * Create a reversed version of the "StandUpBack" animation.
+         */
+        AnimControl animControl = sinbadModelRoot.getControl(AnimControl.class);
+        Animation forward = animControl.getAnim("StandUpBack");
+        float duration = forward.getLength();
+        String reverseName = "LieDown";
+        Animation reverse = new Animation(reverseName, duration);
+        Track[] forwardTracks = forward.getTracks();
+        for (Track forwardTrack : forwardTracks) {
+            Track newTrack = TrackEdit.reverse(forwardTrack);
+            reverse.addTrack(newTrack);
+        }
+        animControl.addAnim(reverse);
+        /*
+         * Add an animation channel.
+         */
+        AnimChannel animChannel = animControl.createChannel();
+        /*
+         * Play the reversed "StandUpBack" animation.
+         */
+        animChannel.setAnim(reverseName);
+    }
+
+    /**
+     * Add application-specific hotkey bindings and override existing ones.
+     */
+    @Override
+    public void moreDefaultBindings() {
+        InputMode dim = getDefaultInputMode();
+
+        dim.bind("dump scenes", KeyInput.KEY_P);
+        dim.bind("signal orbitLeft", KeyInput.KEY_LEFT);
+        dim.bind("signal orbitRight", KeyInput.KEY_RIGHT);
+        dim.bind("toggle pause", KeyInput.KEY_PERIOD);
+        dim.bind("toggle skeleton", KeyInput.KEY_V);
+    }
+
+    /**
+     * Process an action that wasn't handled by the active input mode.
+     *
+     * @param actionString textual description of the action (not null)
+     * @param ongoing true if the action is ongoing, otherwise false
+     * @param tpf time interval between frames (in seconds, &ge;0)
+     */
+    @Override
+    public void onAction(String actionString, boolean ongoing, float tpf) {
+        if (ongoing) {
+            switch (actionString) {
+                case "dump scenes":
+                    dumpScenes();
+                    return;
+                case "toggle pause":
+                    togglePause();
+                    return;
+                case "toggle skeleton":
+                    toggleSkeleton();
+                    return;
+            }
+        }
+        super.onAction(actionString, ongoing, tpf);
+    }
+    // *************************************************************************
+    // private methods
+
+    /**
+     * Add a large static box to serve as a platform.
+     */
+    private void addBox() {
+        float halfExtent = 50f; // mesh units
+        Mesh mesh = new Box(halfExtent, halfExtent, halfExtent);
+        Geometry geometry = new Geometry("box", mesh);
+        rootNode.attachChild(geometry);
+
+        geometry.move(0f, -halfExtent, 0f);
+        ColorRGBA color = new ColorRGBA(0f, 0.2f, 0f, 1f);
+        Material material = MyAsset.createShadedMaterial(assetManager, color);
+        geometry.setMaterial(material);
+        geometry.setShadowMode(RenderQueue.ShadowMode.Receive);
+    }
+
+    /**
+     * Add lighting and shadows to the scene.
+     */
+    private void addLighting() {
+        ColorRGBA ambientColor = new ColorRGBA(0.2f, 0.2f, 0.2f, 1f);
+        AmbientLight ambient = new AmbientLight(ambientColor);
+        rootNode.addLight(ambient);
+
+        Vector3f direction = new Vector3f(1f, -2f, -2f).normalizeLocal();
+        DirectionalLight sun = new DirectionalLight(direction);
+        rootNode.addLight(sun);
+
+        DirectionalLightShadowRenderer dlsr
+                = new DirectionalLightShadowRenderer(assetManager, 4_096, 3);
+        dlsr.setLight(sun);
+        dlsr.setShadowIntensity(0.5f);
+        viewPort.addProcessor(dlsr);
+    }
+
+    /**
+     * Attach a Sinbad model to the root node.
+     */
+    private void addSinbad() {
+        sinbadModelRoot = (Node) assetManager.loadModel(
+                "Models/Sinbad/Sinbad.mesh.xml");
+        rootNode.attachChild(sinbadModelRoot);
+
+        List<Spatial> list
+                = MySpatial.listSpatials(sinbadModelRoot, Spatial.class, null);
+        for (Spatial spatial : list) {
+            spatial.setShadowMode(RenderQueue.ShadowMode.Cast);
+        }
+        setHeight(sinbadModelRoot, 2f);
+        center(sinbadModelRoot);
+        /*
+         * Add a skeleton visualizer.
+         */
+        SkeletonControl sc = sinbadModelRoot.getControl(SkeletonControl.class);
+        sv = new SkeletonVisualizer(assetManager, sc);
+        rootNode.addControl(sv);
+        sv.setLineColor(ColorRGBA.Yellow); // TODO clean up visualization
+    }
+
+    /**
+     * Translate a model's center so that the model rests on the X-Z plane, and
+     * its center lies on the Y axis.
+     */
+    private void center(Spatial model) {
+        Vector3f[] minMax = MySpatial.findMinMaxCoords(model);
+        Vector3f center = MyVector3f.midpoint(minMax[0], minMax[1], null);
+        Vector3f offset = new Vector3f(center.x, minMax[0].y, center.z);
+
+        Vector3f location = model.getWorldTranslation();
+        location.subtractLocal(offset);
+        MySpatial.setWorldLocation(model, location);
+    }
+
+    /**
+     * Configure the camera during startup.
+     */
+    private void configureCamera() {
+        flyCam.setDragToRotate(true);
+        flyCam.setMoveSpeed(4f);
+
+        cam.setLocation(new Vector3f(2.85f, 3.46f, 5.29f));
+        cam.setRotation(new Quaternion(-0.054f, 0.946336f, -0.2308f, -0.2197f));
+
+        CameraOrbitAppState orbitState
+                = new CameraOrbitAppState(cam, "orbitLeft", "orbitRight");
+        stateManager.attach(orbitState);
+    }
+
+    /**
+     * Process a "dump scenes" action.
+     */
+    private void dumpScenes() {
+        Dumper dumper = new Dumper();
+        //dumper.setDumpBucket(true);
+        //dumper.setDumpCull(true);
+        //dumper.setDumpMatParam(true);
+        //dumper.setDumpOverride(true);
+        //dumper.setDumpShadow(true);
+        dumper.setDumpTransform(true);
+        //dumper.setDumpUser(true);
+        dumper.dump(renderManager);
+    }
+
+    /**
+     * Scale the specified model uniformly so that it has the specified height.
+     *
+     * @param model (not null, modified)
+     * @param height (in world units)
+     */
+    private void setHeight(Spatial model, float height) {
+        Vector3f[] minMax = MySpatial.findMinMaxCoords(model);
+        float oldHeight = minMax[1].y - minMax[0].y;
+
+        model.scale(height / oldHeight);
+    }
+
+    /**
+     * Toggle all animations: paused/running.
+     */
+    private void togglePause() {
+        float newSpeed = (speed > 1e-12f) ? 1e-12f : 1f;
+        setSpeed(newSpeed);
+    }
+
+    /**
+     * Toggle the skeleton visualizer on/off.
+     */
+    private void toggleSkeleton() {
+        boolean enabled = sv.isEnabled();
+        sv.setEnabled(!enabled);
+    }
+}
