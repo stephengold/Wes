8d50c797a7f027aa2efb6d87895ad2dada93610a
stephengold <sgold@sonic.net>
2026-08-26 02:55:21 -0700

    AppChooser: override the renderer in AppSettings

--- WesExamples/src/main/java/jme3utilities/test/wes/AppChooser.java
+++ WesExamples/src/main/java/jme3utilities/test/wes/AppChooser.java
@@ -138,6 +138,7 @@ final class AppChooser extends AcorusDemo {
         boolean loadDefaults = true;
         AppSettings settings = new AppSettings(loadDefaults);
         settings.setAudioRenderer(null);
+        settings.setRenderer(AppSettings.LWJGL_OPENGL32); // default=ANGLE_GLES3
         settings.setResizable(true);
         settings.setSamples(4); // anti-aliasing
         settings.setTitle(title); // Customize the window's title bar.
