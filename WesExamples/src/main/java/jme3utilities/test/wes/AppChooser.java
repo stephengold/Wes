a8933d0ae4ddbb53248b2e2d087be30db7f1eaa8
stephengold <sgold@sonic.net>
2026-08-26 02:34:39 -0700

    AppChooser: more detail in javadoc

--- WesExamples/src/main/java/jme3utilities/test/wes/AppChooser.java
+++ WesExamples/src/main/java/jme3utilities/test/wes/AppChooser.java
@@ -310,7 +310,8 @@ final class AppChooser extends AcorusDemo {
      * Look for the shell script (or batch file) to execute in
      * "./build/install/WesExamples/bin".
      *
-     * @return a new instance
+     * @return a new file instance that refers to a pre-existing script file
+     * (not null)
      */
     private static File findScriptToExecute() {
         File buildDir = new File("build");
